3929f7f74eb7548de61191db656f27ed2cb6cf55
kotoar <kotoar@kotoar.com>
2022-11-15 22:36:26 +0000

    add pages UI

---
+++ src/pages/password-reset/ContainerPasswordReset.tsx
@@ -0,0 +1,38 @@
+import {PasswordResetEntryPage} from "./PasswordResetEntryPage";
+import '../__pages.css'
+import React, {useEffect, useState} from "react";
+import {PasswordResetNewPasswordPage} from "./PasswordResetNewPasswordPage";
+import {PasswordResetSuccessPage} from "./PasswordResetSuccessPage";
+import cipraLogo from '../../res/cipra_logo.png'
+
+export enum PasswordResetPageEnum {
+    Entry,
+    NewPassword,
+    Success
+}
+
+export function ContainerPasswordReset(): JSX.Element {
+
+    const [showPage, setShowPage] = useState<PasswordResetPageEnum>(PasswordResetPageEnum.Entry)
+    const [showPageContent, setShowPageContent] = useState<JSX.Element>()
+
+    useEffect(() => {
+        const getShowPage = (pageEnum: PasswordResetPageEnum): JSX.Element => {
+            switch (pageEnum){
+                case PasswordResetPageEnum.Entry:
+                    return <PasswordResetEntryPage trigger={setShowPage} />
+                case PasswordResetPageEnum.NewPassword:
+                    return <PasswordResetNewPasswordPage trigger={setShowPage} />
+                case PasswordResetPageEnum.Success:
+                    return <PasswordResetSuccessPage />
+            }
+            return <div></div>
+        }
+        setShowPageContent(getShowPage(showPage))
+    }, [showPage])
+
+
+    return <div className='center-screen'>
+        {showPageContent}
+    </div>
+}
--- src/pages/password-reset/PasswordResetEntryPage.tsx
+++ src/pages/password-reset/PasswordResetEntryPage.tsx
@@ -1,5 +1,42 @@
-export function PasswordResetEntryPage(): JSX.Element {
+import {PasswordResetPageEnum} from "./ContainerPasswordReset";
+import cipraLogo from "../../res/cipra_logo.png";
+import {Button, Input, SpaceBetween} from "@cloudscape-design/components";
+import {useState} from "react";
+import {ResetPasswordData} from "../../data/ResetPasswordData";
+
+export function PasswordResetEntryPage(props: {trigger: (pageEnum: PasswordResetPageEnum) => void}): JSX.Element {
+
+    const [emailValue, setEmailValue] = useState<string>(ResetPasswordData.instance.email)
+    const [emailTip, setEmailTip] = useState<string>('')
+
+    function submit(): void {
+        ResetPasswordData.instance.email = emailValue
+        ResetPasswordData.instance.emailExist(
+            () => props.trigger(PasswordResetPageEnum.NewPassword),
+            () => setEmailTip("Cannot find account with email address " + emailValue)
+        )
+    }
+
     return <div className="glow-border page-container center-screen" style={{maxWidth: "330px"}}>
-        Password Reset
+        <SpaceBetween size='m' direction='vertical'>
+            <img src={cipraLogo} alt='cipra logo' width='90%' className='line-align-center'/>
+            <h2 className='line-align-center'>Reset Password</h2>
+            <SpaceBetween size='m' direction='horizontal'>
+                <div>Email</div>
+            </SpaceBetween>
+            <Input onChange={({detail}) => setEmailValue(detail.value)}
+                   value={emailValue}
+                   type="email"
+                   placeholder="Email Address"
+            ></Input>
+            {
+                (emailTip.length !== 0) && <p className='tip-text'>{emailTip}</p>
+            }
+            <div className='line-align-center'>
+                <Button variant='primary' onClick={submit}>
+                    Reset Password
+                </Button>
+            </div>
+        </SpaceBetween>
     </div>
 }
